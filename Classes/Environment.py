5dc51eaf6a4e1b3cd0c7574c7c55a20f7d9b8c7c
Kian Bianco <kianbianco.lc@gmail.com>
2024-07-12 14:48:30 -0700

    new file

---
+++ Classes/Environment.py
@@ -0,0 +1,20 @@
+class Environment:
+     def __init__(self), 
+        pass
+
+    friction_coefficients = {
+        'dry_asphalt': 1.0,
+        'wet_asphalt': 0.7,
+        'dry_concrete': 1.0,
+        'wet_concrete': 0.65,
+        'ice': 0.1,
+        'snow': 0.2,
+        'gravel': 0.6,
+        'sand': 0.4,
+        'mud': 0.4,
+        'grass': 0.35,
+        'rubbered_asphalt': 1.1  # Asphalt with rubber buildup from racing
+    }   
+
+    def __repr__(self):
+        pass
